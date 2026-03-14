2bd83698cebb514a05268ffb2fa2bc96b739e33e
marco <marco@DESKTOP-GCUIOA1>
2026-03-14 22:46:41 +0200

    Add project files.

---
+++ G-NET-12-OOP06/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace G_NET_12_OOP06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
